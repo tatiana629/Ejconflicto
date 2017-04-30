fcb6b10e27179f0d022fe172b6cec110e8184bdf
tatiana629 <tatiana629@javerianacali.edu.co>
2017-04-30 16:37:18 -0500

    Initial commit

---
+++ Python.py
@@ -0,0 +1,9 @@
+def nombre_completo():
+    a= (input("ingresa tu primer nombre "))
+    b= (input("ingresa tu segundo nombre "))
+    c= (input("ingresa tu primer apellido "))
+
+    S= a+b+c
+    print ("Nombre completo: ")
+
+    return S

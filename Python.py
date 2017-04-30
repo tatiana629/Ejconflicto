6b046283c861bac8d14507be74736bf51461c9aa
tatiana629 <tatiana629@javerianacali.edu.co>
2017-04-30 16:45:47 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -2,8 +2,9 @@ def nombre_completo():
     a= (input("ingresa tu primer nombre "))
     b= (input("ingresa tu segundo nombre "))
     c= (input("ingresa tu primer apellido "))
+    d= (input("ingresa tu segundo apellido "))
 
-    S= a+b+c
+    S= a+b+c+d
     print ("Nombre completo: ")
 
     return S
